8b6b8cb88a5d55e931c2ee8c90beeaeb2b4db22d
bote795 <nbotell2@gmail.com>
2016-04-28 18:20:41 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict'
+const http = require('http')
+const Bot = require('messenger-bot')
+const auth = require('./credentials.js')
+
+let bot = new Bot({
+  token: process.env.token || auth.token,
+  verify: 'VERIFY_TOKEN'})
+
+bot.on('error', (err) => {
+  console.log(err.message)
+})
+
+let responses = [ "Maybe someday",
+    "Nothing",
+    "Neither",
+    "I don't think so",
+    "No",
+    "Yes",
+    "Try asking again",
+    "NO THIS IS PATRICK!",
+]
+
+
+bot.on('message', (payload, reply) => {
+  let logText = payload.message.text
+  let replyText =""
+  var randomNumber = Math.floor(Math.random()*responses.length)
+
+  replyText= responses[randomNumber]
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text: replyText  }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${logText}`)
+    })
+  })
+})
+let port = process.env.PORT || 3000;
+http.createServer(bot.middleware()).listen(port, "0.0.0.0")
+console.log('Echo bot server running at port '+port+' .')
